546fc842340625dce67987b86c2d5dbf499b8b93
Najmus Seemab <najmus.seemab@gmail.com>
2019-11-18 19:15:07 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+ <footer class="foo">
+            <div class="row">
+                <div class="col-lg-12">
+                    <p style="text-align:center; font-family: 'Monotype Corsiva'; font-size:17px;"><i class="material-icons"
+					style="color: brown;">COPYRIGHT</i> 2019-20 <b ><a href="https://vvce.ac.in">VVCE</a></b></p>
+                </div>
+            </div>
+   </footer>
